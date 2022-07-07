e293333eaceeb637d7ef3ce45bfc0a6bd40dd35a
tomo <sunnyday20200101@gmail.com>
2022-07-07 23:50:45 +0900

    フォームの設置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kaigan';
+  today: Date = new Date();
+  selectedDate = new FormControl();
+  selectedClubs = new FormControl('');
+  selectedActions = new FormControl('');
+  selectedBodyParts = new FormControl('');
+
+  clubList: string[] = [
+    'クラブ問わず',
+    'ウッド',
+    'アイアン',
+    'パター',
+    'ウェッジ',
+    'ユーティリティ',
+    'その他クラブ',
+  ];
+  actionList: string[] = [
+    'アドレス',
+    'テイクバック',
+    'トップ',
+    'ダウンスイング',
+    'インパクト',
+    'フォロー',
+    'フィニッシュ',
+  ];
+  bodyPartsList: string[] = [
+    '頭',
+    '手',
+    '腕',
+    '肩',
+    '胸',
+    '背中',
+    '腹',
+    '腰',
+    '足',
+    '膝',
+    '足裏',
+  ];
+  cons() {
+    console.log(this.selectedClubs);
+    console.log(this.selectedBodyParts);
+    console.log(this.selectedActions);
+    console.log(this.selectedDate);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
