ecb1013424c43bf935f5c95d9739f725ee80e686
tomo <sunnyday20200101@gmail.com>
2022-07-08 23:57:08 +0900

    フォーム追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,12 @@ export class AppComponent {
   title = 'kaigan';
   today: Date = new Date();
   selectedDate = new FormControl();
+  selectedPlace = new FormControl();
   selectedClubs = new FormControl('');
   selectedActions = new FormControl('');
   selectedBodyParts = new FormControl('');
-
+  textArea = new FormControl('');
+  placeList: string[] = ['ラウンド', 'ショートコース', 'レンジ', '自宅'];
   clubList: string[] = [
     'クラブ問わず',
     'ウッド',
@@ -31,6 +33,7 @@ export class AppComponent {
     'インパクト',
     'フォロー',
     'フィニッシュ',
+    'その他のアクション',
   ];
   bodyPartsList: string[] = [
     '頭',
@@ -44,8 +47,11 @@ export class AppComponent {
     '足',
     '膝',
     '足裏',
+    'その他の部位',
   ];
   cons() {
+    console.log(this.textArea);
+    console.log(this.selectedPlace);
     console.log(this.selectedClubs);
     console.log(this.selectedBodyParts);
     console.log(this.selectedActions);
